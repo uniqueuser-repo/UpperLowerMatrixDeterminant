86099646284f8df94507c18772abcdabe9d6ab6a
uniqueuser-repo <57453461+uniqueuser-repo@users.noreply.github.com>
2019-11-06 11:50:44 -0500

    Added Implementations
    Adding the implementations of the structure.

--- JUnit_Tests.java
+++ JUnit_Tests.java
@@ -7,5 +7,28 @@ public class JUnit_Tests {
 
     @Rule
     public Timeout globalTimeout = new Timeout(2);
-    
+
+    @Test
+    public void test_one_upper() {
+        int calculatedValue = UpperLowerMatrixDeterminant.calculate(new int[][] {
+                {1,2,3,4},
+                {0,2,3,4},
+                {0,0,4,5},
+                {0,0,0,9}});
+
+        assertEquals("Ensure that the calculated value was correct!", (1 * 2 * 4 * 9), calculatedValue);
+    }
+
+    @Test
+    public void test_two_lower() {
+        int calculatedValue = UpperLowerMatrixDeterminant.calculate(new int[][] {
+                {9,0,0,0},
+                {1,9,0,0},
+                {4,3,2,0},
+                {1,2,3,4}});
+
+        assertEquals("Ensure that the calculated value was correct!", (9 * 9 * 2 * 4), calculatedValue);
+
+    }
+
 }
--- UpperLowerMatrixDeterminant.java
+++ UpperLowerMatrixDeterminant.java
@@ -1,5 +1,14 @@
 public class UpperLowerMatrixDeterminant {
     public static int calculate(int[][] input) {
 
+        /* no error handling, assumes that the input is correct */
+        int counter = input[0][0];
+
+        for (int i = 1; i < input.length; i++) {
+            counter *= input[i][i];
+        }
+
+        return counter;
+
     }
 }
